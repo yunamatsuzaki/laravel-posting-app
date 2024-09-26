db9dfc1bfab7653a631e58f44a8786e016be73cc
yunamatsuzaki <yunamatsuzaki@icloud.com>
2024-09-26 13:41:38 +0900

    Twitter風アプリに機能を追加しよう

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+ 
+ @section('title', '投稿一覧')
+ 
+ @section('content')
+     @if (session('flash_message'))
+         <p class="text-success">{{ session('flash_message') }}</p>
+     @endif
+ 
+     @if (session('error_message'))
+         <p class="text-danger">{{ session('error_message') }}</p>
+     @endif
+ 
+     <div class="mb-2">
+         <a href="{{ route('posts.create') }}" class="text-decoration-none">新規投稿</a>
+     </div>
+ 
+     @if($posts->isNotEmpty())
+         @foreach($posts as $post)
+             <article>
+                 <div class="card mb-3">
+                     <div class="card-body">
+                         <h2 class="card-title fs-5">{{ $post->title }}</h2>
+                         <p class="card-text">{{ $post->content }}</p>
+                         <p>{{ $post->updated_at }}</p>
+ 
+                         <div class="d-flex">
+                             <a href="{{ route('posts.show', $post) }}" class="btn btn-outline-primary d-block me-1">詳細</a>
+                             <a href="{{ route('posts.edit', $post) }}" class="btn btn-outline-primary d-block me-1">編集</a>
+ 
+                             <form action="{{ route('posts.destroy', $post) }}" method="POST" onsubmit="return confirm('本当に削除してもよろしいですか？');">
+                                 @csrf
+                                 @method('DELETE')
+                                 <button type="submit" class="btn btn-outline-danger">削除</button>
+                             </form>
+                         </div>
+                     </div>
+                 </div>
+             </article>
+         @endforeach
+     @else
+         <p>投稿はありません。</p>
+     @endif
+ @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,11 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index'])->middleware(['auth', 'verified'])->name('posts.index');
 
-Route::get('/dashboard', function () {
+/*Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
@@ -26,6 +26,25 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+});*/
 
 require __DIR__.'/auth.php';
+
+/*
+Route::get('/posts', [PostController::class, 'index'])->middleware(['auth', 'verified'])->name('posts.index');
+
+Route::get('/posts/create', [PostController::class, 'create'])->middleware(['auth', 'verified'])->name('posts.create');
+
+Route::post('/posts', [PostController::class, 'store'])->middleware(['auth', 'verified'])->name('posts.store');
+
+Route::get('/posts/{post}', [PostController::class, 'show'])->middleware(['auth', 'verified'])->name('posts.show');
+
+Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->middleware(['auth', 'verified'])->name('posts.edit');
+
+Route::patch('/posts/{post}', [PostController::class, 'update'])->middleware(['auth', 'verified'])->name('posts.update');
+
+Route::delete('/posts/{post}', [PostController::class, 'destroy'])->middleware(['auth', 'verified'])->name('posts.destroy');
+ */
+
+ Route::resource('posts', PostController::class)->middleware(['auth', 'verified']);
+
